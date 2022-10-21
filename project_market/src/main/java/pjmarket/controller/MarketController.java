eacce4185fe76a3b7a58fcf863e9ae4bd044dbfb
Subgomdore <joy2is@naver.com>
2022-10-21 18:42:08 +0900

    원본파일

---
+++ project_market/src/main/java/pjmarket/controller/MarketController.java
@@ -0,0 +1,22 @@
+package pjmarket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pjmarket.service.MarketService;
+
+@Controller
+public class MarketController {
+
+	@Autowired
+	private MarketService ms;
+
+	// 메인페이지
+	@RequestMapping("mainpage")
+	public String MainPage() {
+		return "main/mainpage";
+	}
+
+}
